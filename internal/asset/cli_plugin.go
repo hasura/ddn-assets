be1acf93ed91aca621937e7ffd1160fe53e9ea03
Vishnu Bharathi <vishnu@hasura.io>
2024-10-30 10:01:22 +0530

    make test pass

--- internal/asset/cli_plugin.go
+++ internal/asset/cli_plugin.go
@@ -1,17 +1,102 @@
 package asset
 
-import "gopkg.in/yaml.v3"
+import (
+	"gopkg.in/yaml.v3"
+)
 
 // CLI plugins are packaged according to https://github.com/hasura/ndc-hub/blob/main/rfcs/0011-cli-and-connector-packaging.md
 
 type ConnectorMetadataYAML struct {
+	Version   string              `yaml:"version"`
+	CLIPlugin CLIPluginDefinition `yaml:"cliPlugin"`
+}
+
+func (cmy *ConnectorMetadataYAML) UnmarshalYAML(value *yaml.Node) error {
+	var temp struct {
+		Version   string `yaml:"version"`
+		CLIPlugin struct {
+			Type                              CLIPluginType `yaml:"type"`
+			DockerCLIPluginDefinition         `yaml:",inline"`
+			BinaryInlineCLIPluginDefinition   `yaml:",inline"`
+			BinaryExternalCLIPluginDefinition `yaml:",inline"`
+		} `yaml:"cliPlugin"`
+	}
+	if err := value.Decode(&temp); err != nil {
+		return err
+	}
+
+	cmy.Version = temp.Version
+
+	plgType := temp.CLIPlugin.Type
+	if !plgType.Valid() {
+		plgType = Binary
+	}
+
+	switch plgType {
+	case Docker:
+		cmy.CLIPlugin = &temp.CLIPlugin.DockerCLIPluginDefinition
+	case BinaryInline:
+		cmy.CLIPlugin = &temp.CLIPlugin.BinaryInlineCLIPluginDefinition
+	case Binary:
+		fallthrough
+	default:
+		cmy.CLIPlugin = &temp.CLIPlugin.BinaryExternalCLIPluginDefinition
+	}
+
+	return nil
+}
+
+type CLIPluginType string
+
+var (
+	Binary       CLIPluginType = "Binary"
+	BinaryInline CLIPluginType = "BinaryInline"
+	Docker       CLIPluginType = "Docker"
+)
+
+func (c CLIPluginType) Valid() bool {
+	switch c {
+	case Binary, BinaryInline, Docker:
+		return true
+	default:
+		return false
+	}
+}
+
+type CLIPluginDefinition interface {
+	GetType() CLIPluginType
+}
+
+type BinaryExternalCLIPluginDefinition struct {
+	Name    string `yaml:"name"`
 	Version string `yaml:"version"`
 }
 
-type BinaryCliPluginDefinition struct {
+type BinaryInlineCLIPluginDefinition struct {
+	Platforms []BinaryCLIPluginPlatform `yaml:"platforms"`
 }
 
-type BinaryInlineCliPluginDefinition struct {
+type DockerCLIPluginDefinition struct {
+	DockerImage string `yaml:"dockerImage"`
+}
+
+func (*BinaryExternalCLIPluginDefinition) GetType() CLIPluginType {
+	return Binary
+}
+
+func (*BinaryInlineCLIPluginDefinition) GetType() CLIPluginType {
+	return BinaryInline
+}
+
+func (*DockerCLIPluginDefinition) GetType() CLIPluginType {
+	return Docker
+}
+
+type BinaryCLIPluginPlatform struct {
+	Selector string
+	URI      string
+	SHA256   string
+	Bin      string
 }
 
 func ParseConnectorMetadata(data []byte) (*ConnectorMetadataYAML, error) {
--- internal/asset/cli_plugin_test.go
+++ internal/asset/cli_plugin_test.go
@@ -4,26 +4,69 @@ import "testing"
 
 func TestParseConnectorMetadata(t *testing.T) {
 	tt := []struct {
+		Name     string
 		RawYAML  []byte
 		Expected *ConnectorMetadataYAML
 	}{
 		{
-			RawYAML: []byte(`version: v1`),
+			Name: "Binary CLI plugin",
+			RawYAML: []byte(
+				`
+version: v2
+cliPlugin:
+  name: ndc-postgres
+  version: v1.2.0
+`),
 			Expected: &ConnectorMetadataYAML{
-				Version: "v1",
+				Version: "v2",
+				CLIPlugin: &BinaryExternalCLIPluginDefinition{
+					Name:    "ndc-postgres",
+					Version: "v1.2.0",
+				},
 			},
 		},
 	}
 
 	for _, tc := range tt {
-		connMetadata, err := ParseConnectorMetadata(tc.RawYAML)
-		if err != nil {
-			t.Error(err)
-			continue
-		}
-		if *connMetadata != *tc.Expected {
-			t.Errorf("expected %+v, got %+v", connMetadata, tc.Expected)
-			continue
-		}
+		t.Run(tc.Name, func(t *testing.T) {
+			connMetadata, err := ParseConnectorMetadata(tc.RawYAML)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			if tc.Expected == nil && connMetadata != nil {
+				t.Error("expected connector metadata to be nil")
+				return
+			}
+
+			if connMetadata.Version != tc.Expected.Version {
+				t.Errorf("expected version %s, got %s", tc.Expected.Version, connMetadata.Version)
+				return
+			}
+
+			if connMetadata.CLIPlugin.GetType() != tc.Expected.CLIPlugin.GetType() {
+				t.Errorf("expected cli plugin type %s, got %s", tc.Expected.CLIPlugin.GetType(), connMetadata.CLIPlugin.GetType())
+			}
+
+			switch expected := tc.Expected.CLIPlugin.(type) {
+			case *BinaryExternalCLIPluginDefinition:
+				got, ok := connMetadata.CLIPlugin.(*BinaryExternalCLIPluginDefinition)
+				if !ok {
+					t.Error("expected to infer plugin type to be external binary cli plugin")
+					return
+				}
+				if got.Name != expected.Name {
+					t.Errorf("expected name %s, got %s", expected.Name, got.Name)
+					return
+				}
+				if got.Version != expected.Version {
+					t.Errorf("expected version %s, got %s", expected.Version, got.Version)
+					return
+				}
+			case *BinaryInlineCLIPluginDefinition:
+			case *DockerCLIPluginDefinition:
+			}
+		})
 	}
 }
