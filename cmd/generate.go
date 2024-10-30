0cbd51f309dec35a878e6c4c74a0c3ae6f651ae8
Vishnu Bharathi <vishnu@hasura.io>
2024-10-30 10:16:17 +0530

    refactor

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,13 +1,9 @@
 package cmd
 
 import (
-	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/fs"
-	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,7 +11,6 @@ import (
 	"github.com/hasura/ddn-assets/internal/asset"
 	"github.com/hasura/ddn-assets/internal/ndchub"
 	"github.com/spf13/cobra"
-	"golang.org/x/sync/errgroup"
 )
 
 var generateCmd = &cobra.Command{
@@ -94,82 +89,11 @@ var generateCmd = &cobra.Command{
 			return
 		}
 
-		var connectorTarball errgroup.Group
-		for _, cp := range connectorPackaging {
-			versionFolder := asset.VersionFolder(cp.Namespace, cp.Name, cp.Version)
-			err = os.MkdirAll(versionFolder, 0777)
-			if err != nil {
-				fmt.Println("error creating folder:", versionFolder, err)
-				os.Exit(1)
-			}
-
-			connectorTarball.Go(func() error {
-				var err error
-				tarballPath := filepath.Join(versionFolder, "connector-definition.tar.gz")
-
-				sha, _ := getSHAIfFileExists(tarballPath)
-				if sha == cp.Checksum.Value {
-					fmt.Println("checksum matched, so using an existing copy: ", tarballPath)
-					return nil
-				}
-
-				defer func() {
-					if err != nil {
-						fmt.Println("error while creating: ", tarballPath)
-						return
-					}
-					sha, _ := getSHAIfFileExists(tarballPath)
-					fmt.Printf("successfully wrote: %s (sha256: %s) \n", tarballPath, sha)
-				}()
-
-				outFile, err := os.Create(tarballPath)
-				if err != nil {
-					return err
-				}
-				defer outFile.Close()
-
-				log.Println("starting download: ", cp.URI)
-				resp, err := http.Get(cp.URI)
-				if err != nil {
-					return err
-				}
-				defer resp.Body.Close()
-
-				if resp.StatusCode != http.StatusOK {
-					return fmt.Errorf("error downloading: status code %d", resp.StatusCode)
-				}
-
-				_, err = io.Copy(outFile, resp.Body)
-				if err != nil {
-					return err
-				}
-				return nil
-			})
-		}
-
-		err = connectorTarball.Wait()
-		if err != nil {
-			fmt.Println("error writing connector tarball", err)
+		if err = ndchub.DownloadConnectorTarballs(connectorPackaging); err != nil {
+			fmt.Println("error downloading connector tarball", err)
 			os.Exit(1)
 		}
 	},
-}
-
-func getSHAIfFileExists(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	hash := sha256.New()
-	_, err = io.Copy(hash, file)
-	if err != nil {
-		return "", err
-	}
-
-	checksum := hash.Sum(nil)
-	return fmt.Sprintf("%x", checksum), nil
 }
 
 func getConnectorMetadata(path string) (*asset.Connector, error) {
--- internal/ndchub/ndc_hub.go
+++ internal/ndchub/ndc_hub.go
@@ -1,10 +1,18 @@
 package ndchub
 
 import (
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/hasura/ddn-assets/internal/asset"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -58,4 +66,77 @@ func GetConnectorPackaging(path string) (*ConnectorPackaging, error) {
 	connectorPackaging.Name = filepath.Base(connectorFolder)
 
 	return &connectorPackaging, nil
+}
+
+func DownloadConnectorTarballs(connPkgs []ConnectorPackaging) error {
+	var connectorTarball errgroup.Group
+	for _, cp := range connPkgs {
+		versionFolder := asset.VersionFolder(cp.Namespace, cp.Name, cp.Version)
+		err := os.MkdirAll(versionFolder, 0777)
+		if err != nil {
+			return fmt.Errorf("error creating folder: %s %w", versionFolder, err)
+		}
+
+		connectorTarball.Go(func() error {
+			var err error
+			tarballPath := filepath.Join(versionFolder, "connector-definition.tar.gz")
+
+			sha, _ := getSHAIfFileExists(tarballPath)
+			if sha == cp.Checksum.Value {
+				fmt.Println("checksum matched, so using an existing copy: ", tarballPath)
+				return nil
+			}
+
+			defer func() {
+				if err != nil {
+					fmt.Println("error while creating: ", tarballPath)
+					return
+				}
+				sha, _ := getSHAIfFileExists(tarballPath)
+				fmt.Printf("successfully wrote: %s (sha256: %s) \n", tarballPath, sha)
+			}()
+
+			outFile, err := os.Create(tarballPath)
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
+
+			log.Println("starting download: ", cp.URI)
+			resp, err := http.Get(cp.URI)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				return fmt.Errorf("error downloading: status code %d", resp.StatusCode)
+			}
+
+			_, err = io.Copy(outFile, resp.Body)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+
+	return connectorTarball.Wait()
+}
+
+func getSHAIfFileExists(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	_, err = io.Copy(hash, file)
+	if err != nil {
+		return "", err
+	}
+
+	checksum := hash.Sum(nil)
+	return fmt.Sprintf("%x", checksum), nil
 }
